2565a26deb63a7b56e4d899a43a92a4b6685d3dd
Paolo Bonomi <paolo.bonomi@outlook.com>
2017-11-26 11:40:48 +0100

    desk

---
+++ Assets/Application/Component/MainCamera/FollowPlayer.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+using System.Collections;
+
+
+namespace Application.Component.MainCamera
+{
+    public class FollowPlayer : MonoBehaviour
+    {
+        private Transform tank_camera;
+
+        public static void attach()
+        {
+            GameObject.Find(Application.MAIN_CAMERA).AddComponent<FollowPlayer>();
+        }
+
+        void Start()
+        {
+            this.tank_camera = GameObject.Find(Application.TANK_CAMERA).transform;    
+        }
+
+        //TODO non permettere rollio
+
+        private void FixedUpdate()
+        {
+            this.transform.position = Vector3.Lerp(this.transform.position, this.tank_camera.position, 0.15f);
+            this.transform.rotation = Quaternion.Lerp(this.transform.rotation, this.tank_camera.rotation, 0.25f);
+        }
+    }
+}
